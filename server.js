2f43949476d42ca949b53fe2fd62226713838557
ISK-Developer <kmg.souvik@gmail.com>
2019-02-22 16:27:34 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+
+app.use(function (req, res, next) {
+    var now = new Date().toString();
+    var log =  `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', function (err) {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    })
+    next();
+});
+
+// app.use(function (req, res, next) {
+//     res.render('maintenence.hbs');
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', function () {
+    return new Date().getFullYear()
+   //return 'test';
+});
+
+hbs.registerHelper('screamIt', function (text) {
+    return text.toUpperCase();
+});
+
+    
+app.get('/', function (req, res) {
+    res.render('home.hbs', {
+    pageTitle: 'Home Page (Dynamic)',
+    currentYear: new Date().getFullYear(),
+    welcomeMsg: 'Welcome to the world of Programming!'
+    });
+});
+
+app.get('/about', function (req, res) {
+  res.render('about.hbs', {
+  pageTitle: 'About Page (Dynamic)',
+  currentYear: new Date().getFullYear()
+});
+});
+
+
+app.get('/bad', function (req, res) {
+    res.send({
+        errorMsg: 'This is a bad request',                
+    })
+    
+});
+
+app.listen(5000, function () {
+    console.log('Server is up on port 5000');
+});
